0e6af164821ac6387d6d5d2e1805b6662c006012
Daniel Thomas <dt7@princeton.edu>
2019-03-19 15:44:22 -0400

    Made Changes to Config File to support user defined analysis

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 #Configuration File
 
+#Do not change, these are ipython notebook demonstration
+
 #Path for data
 dataPath = 'Data/Data_Oct2018_v2.csv'
 
@@ -11,3 +13,20 @@ assetName = ['US Equities','Real Estate','Commodities','Corp Bonds']
 
 #Name of date column
 dateName = 'Date'
+
+#User Analysis Section.  Change the variables in this section to run user specific analysis
+
+#isDemo is a boolean variable, set to True if the user wants to run custom analysis
+isDemo = False
+
+#dataPathUser: Path to User Defined Data
+dataPathUser = 'Data/Data_Oct2018_v2.csv'
+
+#factorNameUser: List, defines the factors
+factorNameUser= ['World Equities','US Treasuries','Bond Risk Premium','Inflation Protection','Currency Protection']
+
+#assetNameUser: List, defines the asset to be used
+assetNameUser = 'US Equities'
+
+#dateName: string, date column
+dateNameUser = 'Date'
